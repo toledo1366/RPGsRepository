36820f330ca56a8619508d75de7f43d1d929969e
damian_sz <damian.szmulik@capgemini.com>
2021-07-06 13:37:43 +0200

    initial commit + creating cards view and cards view model

---
+++ RPGsRepository/RPGsRepository.Android/Views/CardsView.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Platforms.Android.Views;
+using RPGsRepository.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RPGsRepository.Android.Views
+{
+    [Activity(Label = "CardsView")]
+    public class CardsView : MvxActivity<CardsViewModel>
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.CardsView)
+            // Create your application here
+        }
+    }
+}
---
+++ RPGsRepository/RPGsRepository.Core/Models/Card.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPGsRepository.Core.Models
+{
+    public class Card
+    {
+    }
+}
